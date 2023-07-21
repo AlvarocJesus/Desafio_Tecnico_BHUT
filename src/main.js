6fe7b04e2929be3110423f4f3247c7ba7f2db3b5
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2023-07-21 16:50:55 -0300

    Finaliza primeiro endpoint para testar se estava tudo funcionando

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import routes from "./routes";
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 const port = 3000 || process.env.PORT;
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+const { Router } = require("express");
 
 const routes = Router();
 
@@ -6,4 +6,4 @@ routes.get("/", (req, res) => {
 	res.json({ message: "oi" });
 });
 
-export default routes;
+module.exports = routes;
