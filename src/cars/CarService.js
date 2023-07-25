aebf19bfbabfb9932806199ba663c093f659f2fe
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2023-07-25 18:07:48 -0300

    Adiciona estrutura de fila e webhook

--- src/cars/CarService.js
+++ src/cars/CarService.js
@@ -1,14 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const Webhooks = require('node-webhooks');
 const LogsService = require('../Logs/LogsService');
+const Queue = require('../Queue/Queue');
 const CarRepository = require('./CarRepository');
 
 class CarService {
 	constructor() {
 		this.carRepository = new CarRepository();
 		this.logsService = new LogsService();
+		this.queue = new Queue();
 	}
 
 	async listAllCars() {
-		return await this.carRepository.listAllCars();
+		return this.carRepository.listAllCars();
 	}
 
 	async saveCar(car) {
@@ -20,10 +24,27 @@ class CarService {
 			car_id: 2,
 		});
 
+		this.queue.enqueue({
+			id: 1,
+			data_hora: Date.now(),
+			car_id: 2,
+		});
+
+		if (savedCar) {
+			const hooks = this.registerHooks();
+			hooks.trigger('callback_hook', { msg: 'Novo carro adicionado', data: savedCar });
+		}
+
 		return savedCar;
 	}
 
-	async listLogs() {}
+	async registerHooks() {
+		return new Webhooks({
+			db: {
+				callback_hook: ['http://localhost:8005/webhook-client'],
+			},
+		});
+	}
 }
 
 module.exports = CarService;
